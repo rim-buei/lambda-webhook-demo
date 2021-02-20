cc714ae1b53363b4b380f44c4740417b8ed9af0b
Rim <rim.buei@gmail.com>
2021-02-20 14:50:41 +0900

    src/main: add some code to deserialize GitHub webhook event in lambda proxy event

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,59 @@ use std::error::Error;
 use lambda_runtime::{error::HandlerError, lambda, Context};
 use log;
 use serde::{Deserialize, Serialize};
-use simple_error::bail;
 use simple_logger::SimpleLogger;
 
-#[derive(Deserialize)]
-struct CustomEvent {
-    first_name: String,
+#[derive(Debug, Deserialize)]
+struct ProxyRequest {
+    body: String,
 }
 
 #[derive(Serialize)]
-struct CustomOutput {
-    message: String,
+struct ProxyResponse {
+    #[serde(rename = "statusCode")]
+    status_code: u16,
+
+    body: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct WebhookEvent {
+    action: String,
+    pull_request: PullRequestEvent,
+}
+
+#[derive(Debug, Deserialize)]
+struct PullRequestEvent {
+    merged: bool,
+    milestone: Option<u64>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     SimpleLogger::new().init().unwrap();
-    lambda!(handler);
+    lambda!(handle_proxy_event);
 
     Ok(())
 }
 
-fn handler(e: CustomEvent, c: Context) -> Result<CustomOutput, HandlerError> {
-    if e.first_name == "" {
-        log::error!("Empty first name in request {}", c.aws_request_id);
-        bail!("Empty first name");
+fn handle_proxy_event(e: ProxyRequest, c: Context) -> Result<ProxyResponse, HandlerError> {
+    handle_event(serde_json::from_str(&e.body).unwrap(), c)
+}
+
+fn handle_event(e: WebhookEvent, c: Context) -> Result<ProxyResponse, HandlerError> {
+    if e.action != "closed" && !e.pull_request.merged {
+        log::info!(
+            "Ignorering webhook event: event={:?} request_id={}",
+            e,
+            c.aws_request_id
+        );
+        return Ok(ProxyResponse {
+            status_code: 200,
+            body: format!("Webhook event has been ignored: event={:?}", e),
+        });
     }
 
-    Ok(CustomOutput {
-        message: format!("Hello, {}!", e.first_name),
+    Ok(ProxyResponse {
+        status_code: 200,
+        body: format!("PR has been closed: event={:?}", e),
     })
 }
