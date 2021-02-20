6035cd39c19206b36ed3e9933aa3faf01a25fa99
Rim <rim.buei@gmail.com>
2021-02-21 07:34:21 +0900

    src/github: define pull_request field as optional

--- src/github.rs
+++ src/github.rs
@@ -3,18 +3,18 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct WebhookEvent {
     action: String,
-    pull_request: PullRequestEvent,
+    pull_request: Option<PullRequestEvent>,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct PullRequestEvent {
+struct PullRequestEvent {
     merged: bool,
     milestone: Option<u64>,
 }
 
 pub fn handle_webhook(e: WebhookEvent) -> Result<(), String> {
-    if e.action != "closed" && !e.pull_request.merged {
-        log::info!("Ignorering webhook event: event={:?}", e);
+    if e.action != "closed" || e.pull_request.is_none() {
+        log::info!("Ignoring webhook event: event={:?}", e);
         return Ok(());
     }
 
